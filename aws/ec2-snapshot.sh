f639a1ec0ab7fbe55cefb036e53e73c41b9df9de
radsec <39590744+radsec@users.noreply.github.com>
2019-09-09 17:02:47 -0400

    snapshot-aws

--- aws/ec2-snapshot.sh
+++ aws/ec2-snapshot.sh
@@ -9,9 +9,9 @@ else
 	BUILDER=${1}
 fi
 
-PROFILE="playground"
+PROFILE="default"
 
-# print command for configuring the aws profile 
+# print command for configuring the aws profile
 echo "aws configure --profile ${PROFILE}"
 
 BUCKET="ec2-vm-import-3284a153f2ed"
@@ -23,16 +23,16 @@ aws s3 cp --profile ${PROFILE} ../output-${BUILDER}/${IMAGE} s3://${BUCKET}/${IM
 IMAGEFORMAT="$(echo ${IMAGE} | rev | cut -d "." -f1 | rev)"
 IMPORTSNAPSHOT="import-snapshot_${TIMESTAMP}.json"
 
-cp import-snapshot.json ${IMPORTSNAPSHOT}
-sed -i s/FORMAT_PLACEHODLER/${IMAGEFORMAT}/g ${IMPORTSNAPSHOT}
-sed -i s/BUCKET_PLACEHODLER/${BUCKET}/g ${IMPORTSNAPSHOT}
-sed -i s/IMAGE_PLACEHODLER/${IMAGE}/g ${IMPORTSNAPSHOT}
+cp import-snapshot-pf-2-4-4-p3.json ${IMPORTSNAPSHOT}
+gsed -i s/FORMAT_PLACEHODLER/${IMAGEFORMAT}/g ${IMPORTSNAPSHOT}
+gsed -i s/BUCKET_PLACEHODLER/${BUCKET}/g ${IMPORTSNAPSHOT}
+gsed -i s/IMAGE_PLACEHODLER/${IMAGE}/g ${IMPORTSNAPSHOT}
 
 echo "Import image as EC2 snapshot ..."
 # print output to stdout, capture ImporTaskId
 IMPORTTASKID=$(aws ec2 --profile ${PROFILE} import-snapshot --disk-container file://${IMPORTSNAPSHOT} | tee /dev/tty | grep ImportTaskId | cut -d \" -f 4)
 
 # print command for following snapshot import status
-echo aws ec2 --profile ${PROFILE} describe-import-snapshot-tasks --import-task-id ${IMPORTTASKID}
+aws ec2 --profile ${PROFILE} describe-import-snapshot-tasks --import-task-id ${IMPORTTASKID}
 
 rm ${IMPORTSNAPSHOT}
